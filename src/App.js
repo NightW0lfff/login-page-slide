b164576f2000569bf7d5b8527c412ce8e9561078
NightW0lfff <88699008+NightW0lfff@users.noreply.github.com>
2024-03-20 20:05:07 +1100

    Initialised Login Page project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="login-card">
+        <Card_form />
+      </div>
+    </div>
+  );
+}
+
+function Login() {}
+
+function Register() {}
+
+function Card_form() {
+  return (
+    <div className="element-container">
+      <div className="input-container">
+        <h4>User</h4>
+        <input />
+      </div>
+      <div className="input-container">
+        <h4>Password</h4>
+        <input />
+      </div>
+      <button>Login</button>
+      <p>Create an account!</p>
     </div>
   );
 }
